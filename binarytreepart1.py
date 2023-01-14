b4b622c796a99f82303736e770a47583b674abc7
Jacqueline C Bermundo <92629695+jacqbermundo16@users.noreply.github.com>
2023-01-14 11:32:54 +0800

    Printing the string in in-order traversal and it printed the strings in aphabetical order.

--- binarytreepart1.py
+++ binarytreepart1.py
@@ -91,4 +91,6 @@ if __name__ == '__main__':
     print("UK is in the list? ", country_tree.search("UK"))
     print("Sweden is in the list? ", country_tree.search("Sweden"))
 
+    print(country_tree.in_order_traversal())
+
 
