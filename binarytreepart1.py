7409354939deda9e806ecda6b303a787727cc1b5
Jacqueline C Bermundo <92629695+jacqbermundo16@users.noreply.github.com>
2023-01-14 13:35:45 +0800

    Write a function that will determine sum of all the elemenents in the binary tree.

--- binarytreepart1.py
+++ binarytreepart1.py
@@ -74,6 +74,13 @@ class BinarySearchTreeNode:
             return self.data
         return self.right.find_max()
 
+    # find the sum of the elements
+    def calculate_sum(self):
+        left_sum = self.left.calculate_sum() if self.left else 0
+        right_sum = self.right.calculate_sum() if self.right else 0
+        return self.data + left_sum + right_sum
+
+
 
 
 # write a helper method
@@ -112,6 +119,9 @@ if __name__ == '__main__':
 
     # print the minimum element
     print("Min: ", numbers_tree.find_min())
+
+    # print the maximum element
+    print("Min: ", numbers_tree.find_max())
 
 
 
