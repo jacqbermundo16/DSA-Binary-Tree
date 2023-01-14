700de20e630046e24ea9fff86ecd50be2a35811d
Jacqueline C Bermundo <92629695+jacqbermundo16@users.noreply.github.com>
2023-01-14 10:32:10 +0800

    Adding nodes (base, left subtree, right subtree) that also checks if it is empty or not and if it is, we defined an if-else statement that willadd data to either right or left tree with recursion.

--- binarytreepart1.py
+++ binarytreepart1.py
@@ -1,6 +1,27 @@
-#define a binary search tree node class
+# define a binary search tree node class
 class BinarySearchTreeNode:
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
+    # adding nodes
+    def add_child(self, data):
+        # if there data is not empty
+        if data == self.data:
+            return
+
+        if data < self.data:
+            # add data in the left subtree
+            # check if left node has a value
+            if self.left:
+                self.left.add_child(data)
+            # if left node is empty
+            else:
+                self.left = BinarySearchTreeNode(data) 
+        else:
+            # add data in right subtree
+            if self.right:
+                self.right.add_child(data)
+            # if left node is empty
+            else:
+                self.right = BinarySearchTreeNode(data) 
